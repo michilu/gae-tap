92b69ae6668c40cfb7c938c4870d559409b8a616
MiCHiLU <djmchl@gmail.com>
2013-12-22 08:48:27 +0700

    remove comments

--- gae/utils.py
+++ gae/utils.py
@@ -1476,7 +1476,6 @@ class DropboxProxy(RequestHandler):
 class AuthHandler(BaseRequestHandler, SimpleAuthHandler):
   """Authentication handler for OAuth 2.0, 1.0(a) and OpenID."""
 
-  # Enable optional OAuth 2.0 CSRF guard
   OAUTH2_CSRF_STATE = True
 
   USER_ATTRS = {
@@ -1526,10 +1525,6 @@ class AuthHandler(BaseRequestHandler, SimpleAuthHandler):
   }
 
   def _on_signin(self, data, auth_info, provider):
-    """Callback whenever a new or existing user is logging in.
-     data is a user info dictionary.
-     auth_info contains access token or oauth token and secret.
-    """
     auth_id = '%s:%s' % (provider, data['id'])
     logging.info('Looking for a user with id %s', auth_id)
 
@@ -1538,31 +1533,18 @@ class AuthHandler(BaseRequestHandler, SimpleAuthHandler):
 
     if user:
       logging.info('Found existing user to log in')
-      # Existing users might've changed their profile data so we update our
-      # local model anyway. This might result in quite inefficient usage
-      # of the Datastore, but we do this anyway for demo purposes.
-      #
-      # In a real app you could compare _attrs with user's properties fetched
-      # from the datastore and update local user in case something's changed.
       user.populate(**_attrs)
       user.put()
       self.auth.set_session(
         self.auth.store.user_to_dict(user))
 
     else:
-      # check whether there's a user currently logged in
-      # then, create a new user if nobody's signed in,
-      # otherwise add this auth_id to currently logged in user.
 
       if self.logged_in:
         logging.info('Updating currently logged in user')
 
         u = self.current_user
         u.populate(**_attrs)
-        # The following will also do u.put(). Though, in a real app
-        # you might want to check the result, which is
-        # (boolean, info) tuple where boolean == True indicates success
-        # See webapp2_extras.appengine.auth.models.User for details.
         u.add_auth_id(auth_id)
 
       else:
@@ -1571,12 +1553,9 @@ class AuthHandler(BaseRequestHandler, SimpleAuthHandler):
         if ok:
           self.auth.set_session(self.auth.store.user_to_dict(user))
 
-    # Remember auth data during redirect, just for this demo. You wouldn't
-    # normally do this.
     self.session.add_flash(data, 'data - from _on_signin(...)')
     self.session.add_flash(auth_info, 'auth_info - from _on_signin(...)')
 
-    # Go to the profile page
     self.redirect('/profile')
 
   def logout(self):
@@ -1591,11 +1570,9 @@ class AuthHandler(BaseRequestHandler, SimpleAuthHandler):
     return self.uri_for('auth_callback', provider=provider, _full=True)
 
   def _get_consumer_info_for(self, provider):
-    """Returns a tuple (key, secret) for auth init requests."""
     return secrets.AUTH_CONFIG[provider]
 
   def _to_user_model_attrs(self, data, attrs_map):
-    """Get the needed information from the provider dataset."""
     user_attrs = {}
     for k, v in attrs_map.iteritems():
       attr = (v, data.get(k)) if isinstance(v, str) else v(data.get(k))
