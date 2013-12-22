6e85182cf2d82e794979733f3d3199dc7817e2b2
MiCHiLU <djmchl@gmail.com>
2013-12-22 19:13:48 +0700

    look up on_signin callback func from oauth_config

--- gae/utils.py
+++ gae/utils.py
@@ -1554,8 +1554,8 @@ class OAuth(RequestHandler, SimpleAuthHandler):
     return super(OAuth, self)._simple_auth(*argv, **kwargv)
 
   def _on_signin(self, data, auth_info, provider):
-    oauth_id = '%s:%s' % (provider, data['id'])
-    logging.debug('Looking for a user with id %s', oauth_id)
+    if hasattr(self.oauth_config, "on_signin"):
+      self.oauth_config.on_signin(self, data, auth_info, provider)
     for referer, _label in self.session.get_flashes():
       referer = referer.encode("utf-8")
       break
