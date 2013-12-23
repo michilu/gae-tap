50572754efa830acf161fe3e2ea02e213fcef7a6
MiCHiLU <djmchl@gmail.com>
2013-12-23 14:52:08 +0700

    refactor

--- gae/appengine_config.py
+++ gae/appengine_config.py
@@ -9,11 +9,16 @@ config_APPS = {
   #r"<subdomain:(?!www\.)[^.]+>.local": (("/test", "app_sample"),),
 }
 
+# This is a session secret key used by webapp2 framework.
+# Get 'a random and long string' from here:
+# http://clsc.net/tools/random-string-generator.php
+# or execute this from a python shell: import os; os.urandom(64)
+config_SECRET_KEY = "a very long and secret session key goes here"
+
 #config_APPSTATS_INCLUDE_ERROR_STATUS = False
 #config_FEEDBACK_FORMKEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 #config_GA_ACCOUNT = ""
 #config_JOB_EMAIL_RECIPIENT = "email@example.com"
-config_SECRET_KEY = "SECRET_KEY"
 #config_WEBAPP2_CONFIG = {}
 
 def webapp_add_wsgi_middleware(app):
--- gae/oauth_config/sample.py
+++ gae/oauth_config/sample.py
@@ -1,11 +1,5 @@
 # Copy this file into secrets.py and set keys, secrets and scopes.
 
-# This is a session secret key used by webapp2 framework.
-# Get 'a random and long string' from here: 
-# http://clsc.net/tools/random-string-generator.php
-# or execute this from a python shell: import os; os.urandom(64)
-SESSION_KEY = "a very long and secret session key goes here"
-
 # Google APIs
 GOOGLE_APP_ID = 'app id'
 GOOGLE_APP_SECRET = 'app secret'
