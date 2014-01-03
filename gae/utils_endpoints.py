9ae04c52f9b779b83c5e704c32088fdf607d3dd4
MiCHiLU <djmchl@gmail.com>
2014-01-03 18:38:42 +0700

    add utils for endpoints

---
+++ gae/utils_endpoints.py
@@ -0,0 +1,36 @@
+from protorpc import remote
+
+# Google Cloud Endpoints
+
+def get_user_from_endpoints_service(endpoints_service):
+  request_state = endpoints_service.request_state
+  request_state.app = get_app()
+  request_state.cookies = webob.request.RequestCookies({
+    "HTTP_COOKIE": request_state.headers.get("cookie"),
+  })
+  session_store = sessions.SessionStore(request_state)
+  session_store.config["secret_key"] = get_namespaced_secret_key(namespace_manager.get_namespace())
+  return User.load_from_session(session_store.get_session())
+
+class CRUDServiceClass(remote._ServiceClass):
+
+  @staticmethod
+  def __add_prefix(name, dct):
+    new_dct = dict()
+    for key, value in dct.iteritems():
+      if not key.startswith("_"):
+        key = "_{0}_{1}".format(name, key)
+      new_dct[key] = value
+    return new_dct
+
+  def __new__(cls, name, bases, dct):
+    new_dct = CRUDServiceClass.__add_prefix(name, dct)
+    return super(CRUDServiceClass, cls).__new__(cls, name, bases, new_dct)
+
+  def __init__(cls, name, bases, dct):
+    new_dct = CRUDServiceClass.__add_prefix(name, dct)
+    super(CRUDServiceClass, cls).__init__(name, bases, new_dct)
+
+class CRUDService(remote.Service):
+
+  __metaclass__ = CRUDServiceClass
