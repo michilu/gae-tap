9465e0e9e77571c0ffea92a991ef05326436801f
MiCHiLU <djmchl@gmail.com>
2014-01-05 17:17:01 +0700

    refactor, faster

--- tests/conf.py
+++ tests/conf.py
@@ -1,7 +1,4 @@
 import os
 
-root_path = None
-
-if root_path is None:
-  root_path = os.path.dirname(os.path.dirname( __file__ )) + "/gae"
-  assert os.path.exists(root_path)
+root_path = os.path.dirname(os.path.dirname( __file__ )) + "/gae"
+assert os.path.exists(root_path)
