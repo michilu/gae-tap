4bb666d59b60363a4f8d5be5616286f98a387387
ENDOH takanao <djmchl@gmail.com>
2014-09-30 12:53:12 +0700

    fix for wercker environments

--- assets/test_dev_appserver.sh
+++ assets/test_dev_appserver.sh
@@ -6,7 +6,7 @@ timeout_sec=`expr 12 + 8 \* ${random} / 255`
 echo set timeout ${timeout_sec}s...
 
 timeout ${timeout_sec}s $GOOGLE_APPENGINE/dev_appserver.py --skip_sdk_update_check=yes --dev_appserver_log_level=error gae/app.yaml 2>log
-if [ `grep -c "" log` != 0 ]; then
+if [ `grep -c -v ^INFO log` != 0 ]; then
   cat log;
   exit 1;
 fi
