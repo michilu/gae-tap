fbd3cf1bff4cd627cc58ea8c191cc7a72b31f555
ENDOH takanao <djmchl@gmail.com>
2014-09-30 17:03:20 +0700

    remove doc

--- assets/make_non_dependence_environment.sh
+++ assets/make_non_dependence_environment.sh
@@ -2,13 +2,17 @@
 
 echo > gae/site-packages/packages-no-deps.txt
 echo > gae/site-packages/packages.txt
+
 rm -f gae/app_sample.py
 rm -f gae/site-packages/uamobile.py
+
+rm -rf doc
 rm -rf gae/locales
 rm -rf gae/oauth_config
 rm -rf gae/static
 rm -rf gae/static_root
 rm -rf gae/templates
+
 if [ `uname` == "Darwin" ]; then
   sed -i "" "/app_sample/d" gae/appengine_config.py;
 else
